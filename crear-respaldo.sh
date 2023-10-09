04eb62175da1e177211982444ae8c2a92415f7d4
Andrea Monterrossa <moralesandrea.edr4@gmail.com>
2023-10-09 17:13:51 +0000

    cargar respaldo de base de datos y correción cargar respaldo

--- crear-respaldo.sh
+++ crear-respaldo.sh
@@ -5,6 +5,8 @@ set +o allexport
 current_time=$(date +'%Y-%d-%m-%H-%M-%S')
 backup_name=$(echo "respaldo_mysql_${MYSQL_WORDPRESS_DATABASE_NAME}_${current_time%?}.sql")
 mkdir -p $MYSQL_BACKUP_DIR
-docker exec db /usr/bin/mysqldump -u root --password=$MYSQL_ROOT_PASSWORD $MYSQL_WORDPRESS_DATABASE_NAME > $(echo "${MYSQL_BACKUP_DIR}/${backup_name}")
+
+docker exec db sh -c 'exec mysqldump --all-databases -uroot -p"$MYSQL_ROOT_PASSWORD"' > $(echo "${MYSQL_BACKUP_DIR}/${backup_name}")
+#docker exec db /usr/bin/mysqldump -u root --password=$MYSQL_ROOT_PASSWORD $MYSQL_WORDPRESS_DATABASE_NAME > $(echo "${MYSQL_BACKUP_DIR}/${backup_name}")
 printf "%s=> Se realizó respaldo de datos de ${MYSQL_WORDPRESS_DATABASE_NAME}. \n=> Archivo de Respaldo: ${MYSQL_BACKUP_DIR}/${backup_name}.\n"
 
