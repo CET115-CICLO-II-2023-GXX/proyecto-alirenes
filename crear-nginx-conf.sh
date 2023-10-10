da4f6a56fb3f5171ec092cd0829793ece4e1047b
Andrea Monterrosa <moralesandrea.edr4@gmail.com>
2023-10-10 05:19:37 +0000

    reparación variabbles de entorno crear-nginx-conf.sh

--- crear-nginx-conf.sh
+++ crear-nginx-conf.sh
@@ -5,9 +5,9 @@ set +o allexport
 # Credenciales de Nginx
 read -p "Es versión inicial?(y/n)" IS_INIT
 if [ "$IS_INIT" = "y" ]; then
-envsubst < nginx.conf.init.template > ./nginx-conf/nginx.conf
+envsubst '$DOMINIO $WWW_DOMINIO $SV_DOMINIO' < nginx.conf.init.template > ./nginx-conf/nginx.conf
 else
-envsubst < nginx.conf.template > ./nginx-conf/nginx.conf
+envsubst '$DOMINIO $WWW_DOMINIO $SV_DOMINIO'  < nginx.conf.template > ./nginx-conf/nginx.conf
 fi
 #docker-compose up -d
 #docker-compose exec webserver ls -la /etc/letsencrypt/live
