63e4d914b837832d7ec357fdd52108e8532d9b3f
Andrea Monterrossa <moralesandrea.edr4@gmail.com>
2023-10-10 04:50:45 +0000

    corrección archivos nginx

--- crear-nginx-conf.sh
+++ crear-nginx-conf.sh
@@ -3,8 +3,12 @@ set -o allexport
 source .env
 set +o allexport 
 # Credenciales de Nginx
+read -p "Es versión inicial?(y/n)" IS_INIT
+if [ "$IS_INIT" = "y" ]; then
+envsubst < nginx.conf.init.template > ./nginx-conf/nginx.conf
+else
 envsubst < nginx.conf.template > ./nginx-conf/nginx.conf
-
+fi
 #docker-compose up -d
 #docker-compose exec webserver ls -la /etc/letsencrypt/live
 #docker-compose up --force-recreate --no-deps certbot
